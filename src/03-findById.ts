89bbfddffb5d44c57a7b177573eb6ba463386440
lucaslangsch <lucas.langsch@gmail.com>
2023-08-10 14:24:59 -0300

    feat: add function findById

--- src/03-findById.ts
+++ src/03-findById.ts
@@ -1,27 +1,33 @@
-// const products = [
-//   {
-//     id: 1,
-//     name: 'Camiseta',
-//     price: 29.9,
-//     quantity: 3,
-//     brands: ['Nike', 'Reebok'],
-//   },
-//   {
-//     id: 2,
-//     name: 'Caneca',
-//     price: 19.90,
-//     quantity: 12,
-//     brands: ['Stanley', 'Camelbak'],
-//   },
-//   {
-//     id: 3,
-//     name: 'Boné',
-//     price: 39.90,
-//     quantity: 0,
-//     brands: [],
-//   },
-// ];
+import { Product } from './02-insertProducts';
 
-// export default function findById() {
-//   // insira a solução
-// }
+const products: Product[] = [
+  {
+    id: 1,
+    name: 'Camiseta',
+    price: 29.9,
+    quantity: 3,
+    brands: ['Nike', 'Reebok'],
+  },
+  {
+    id: 2,
+    name: 'Caneca',
+    price: 19.90,
+    quantity: 12,
+    brands: ['Stanley', 'Camelbak'],
+  },
+  {
+    id: 3,
+    name: 'Boné',
+    price: 39.90,
+    quantity: 0,
+    brands: [],
+  },
+];
+
+export default function findById(id: number): Product | string {
+  const productById = products.find((product) => product.id === id);
+  if (productById) {
+    return productById;
+  }
+  return `Produto com ID ${id} não encontrado.`;
+}
