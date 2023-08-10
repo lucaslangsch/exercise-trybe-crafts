4875775f097df99b60295bc2c609569f968b8863
lucaslangsch <lucas.langsch@gmail.com>
2023-08-10 14:11:24 -0300

    feat: add function insertProducts

--- src/02-insertProducts.ts
+++ src/02-insertProducts.ts
@@ -1,3 +1,14 @@
-// export default function insertProducts() {
-//   // insira a solução
-// }
+export type Product = {
+  id: number,
+  name: string,
+  price: number,
+  quantity: number,
+  brands: string[],
+};
+
+export default function insertProducts(arr: string[], product: Product): string {
+  if (product.brands.every((brand) => arr.includes(brand))) {
+    return `${product.name} adicionado(a) com o preço de R$ ${product.price}`;
+  }
+  return 'Seu produto contém marcas indisponíveis.';
+}
