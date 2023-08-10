5d06915748c52baa125369830e9f06ae4043c835
lucaslangsch <lucas.langsch@gmail.com>
2023-08-10 14:45:18 -0300

    feat: add function findById

--- src/04-calculateTotalPrice.ts
+++ src/04-calculateTotalPrice.ts
@@ -1,3 +1,16 @@
-// export default function calculateTotalPrice() {
-//   // insira a solução
-// }
+import { Product } from './02-insertProducts';
+
+export type Item = {
+  product: Product,
+  quantity: number,
+  phrase: string,
+  brand: string,
+};
+
+export default function calculateTotalPrice(products: Item[]): number {
+  const sum = products.reduce((total, prod) => (
+    total + prod.product.price * prod.quantity
+  ), 0);
+
+  return parseFloat(sum.toFixed(2));
+}
